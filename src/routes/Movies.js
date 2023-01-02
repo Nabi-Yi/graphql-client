3b7ec0f2eff4a92e6749a5d9fd7ea4c99669a2d4
Nabi-Yi <nabiyi926@gmail.com>
2023-01-03 03:58:30 +0900

    fundamentally implement graphql query with apollo-provider & useState

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,4 +1,29 @@
-export default function Movies(){
-    return <div> This is a list of movies</div>;
+import {gql, useApolloClient} from "@apollo/client";
+import {useEffect, useState} from "react";
+
+export default function Movies() {
+    const [movies, setMovies] = useState([])
+    const client = useApolloClient();
+    useEffect(() => {
+        client.query({
+            query: gql`
+                {
+                    allMovies {
+                        title
+                        id
+                    }
+                }
+            `
+        }).then(response => setMovies(response.data.allMovies))
+    }, [client])
+    return <div>
+        {
+            movies.map((movie) => (
+                <li key = {movie.id}>
+                    {movie.title}
+                </li>
+            ))
+        }
+    </div>;
 }
 
